1e4472e82ddd88b6e6f23fc049f4836f61cd4c6a
projmeloedu <167816622+projmeloedu@users.noreply.github.com>
2024-05-20 18:27:57 -0300

    .
    .

--- rotas.js
+++ rotas.js
@@ -1,4 +1,16 @@
 const express = require('express')
 const app = express()
 
-const alunoDB= reuire(' ./app')
+const alunoDB= require('./app2')
+
+
+app.get('/usuarios', function(req,res){
+    res.type("usuarios.html")
+})
+
+//Iniciar servidor
+let server = app.listen(3000, function(){
+    let host = server.address().address
+    let port = server.address().port
+    console.log("Servidor iniciado em http://%s:%s", host, port)
+})
