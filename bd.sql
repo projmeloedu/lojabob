ea6438c632e1ca228aa02921fd47ae81c62362ee
projmeloedu <167816622+projmeloedu@users.noreply.github.com>
2024-05-27 18:10:18 -0300

    .
    .

--- bd.sql
+++ bd.sql
@@ -8,3 +8,6 @@ create table usuario(
     email varchar(200) null,
     primary key(id)
 )
+
+use bob;
+select * from usuario;
---
+++ teste.js
@@ -0,0 +1,5 @@
+const app = require('./app2')
+
+//app.inserir('uvinha2','laranjinha','tata@gmail.com')
+
+app.apagar(6)
